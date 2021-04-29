a5b63fcbb2f605c6ca7e096e9fdd20532a2c58ab
ckurt <ckurt@student.42lyon.fr>
2021-04-29 13:12:54 +0200

    done : handling double env in lexer

--- src/lexer.c
+++ src/lexer.c
@@ -1,5 +1,20 @@
 #include "../includes/minishell.h"
 
+void	join_last_token(t_token *token)
+{
+	t_list	*lst;
+	t_token	*actual;
+
+	lst = g_shell.tokens;
+	while (lst)
+	{
+		actual = lst->content;
+		if (actual->id == token->id - 1)
+			actual->str = ft_strjoin(actual->str, token->str);
+		lst = lst->next;
+	}
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned int	i;
@@ -43,18 +58,25 @@ void	display_tokens()
 	}
 }
 
-void	get_token_info(t_token *token, char *line, int start, int end)
+int	get_token_info(t_token *token, char *line, int start, int end)
 {
 	token->str = ft_substr(line, start, end - start);
 	if (!ft_strcmp(token->str, "-n"))
 		token->type = 1;
 	else if (token->str[0] == '$')
 	{
-		printf("Last type is [%d]\n", check_type_at(token->id - 1));
+		if (check_type_at(token->id - 1) == 2)
+		{
+			join_last_token(token);
+			printf("Last type is [%d]\n", check_type_at(token->id - 1));
+			token->type = 2;
+			return(2);
+		}
 		token->type = 2;
 	}
 	else
 		token->type = 0;
+	return (0);
 }
 
 void	get_lexer(char *line)
@@ -71,8 +93,8 @@ void	get_lexer(char *line)
 			if (line[lexer.i - 1] != ' ')
 			{
 				token->id = lexer.id++;
-				get_token_info(token, line, lexer.j, lexer.i);
-				ft_lstadd_back(&g_shell.tokens, ft_lstnew(token));
+				if (get_token_info(token, line, lexer.j, lexer.i) != 2)
+					ft_lstadd_back(&g_shell.tokens, ft_lstnew(token));
 			}
 			if (line[lexer.i] == '$')
 				lexer.j = lexer.i;
@@ -86,8 +108,8 @@ void	get_lexer(char *line)
 		if (line[lexer.i - 1])
 		{
 			token->id = lexer.id++;
-			get_token_info(token, line, lexer.j, lexer.i);
-			ft_lstadd_back(&g_shell.tokens, ft_lstnew(token));
+			if (get_token_info(token, line, lexer.j, lexer.i) != 2)
+				ft_lstadd_back(&g_shell.tokens, ft_lstnew(token));
 		}
 	}
 	display_tokens();
