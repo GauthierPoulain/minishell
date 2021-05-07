3610e15e2c514c22bbe92b5f5066b2b2a3d25690
ckurt <ckurt@student.42lyon.fr>
2021-05-07 12:47:55 +0200

    done: way better parsing

--- src/parse_env_var.c
+++ src/parse_env_var.c
@@ -6,19 +6,29 @@ char	*treat_dollar(char *word, int i, int env)
 	char	*env_value;
 	char	*new_word;
 
-	printf("act word = [%s]\n", word);
 	buff = ft_strndup(word, i);
-	printf("buff = [%s]\n", buff);
 	env_value = get_env(word + i + env);
-	printf("env = [%s]\n", env_value);
 	new_word = ft_strjoin(buff, env_value);
-	printf("new = [%s]\n", new_word);
 	if (ft_strlen(buff))
 		new_word = ft_strjoin(new_word, word + ft_strlen(buff) + ft_strlen(env_value));
 	return (new_word);
 }
 
-void	treat_several_dollars(char *word)
+char	*join_tab(char **array)
+{
+	int		i;
+	char	*res;
+
+	i = 1;
+	res = ft_strdup(array[0]);
+	while (array[i])
+		res = ft_strjoin(res, array[i++]);
+	if (DEBUG)
+		printf("res [%s]\n", res);
+	return (res);
+}
+
+char	*treat_several_dollars(char *word)
 {
 	char	**array;
 	int		j;
@@ -27,11 +37,10 @@ void	treat_several_dollars(char *word)
 	array = ft_split(word, '$');
 	while (array[j])
 	{
-		printf("before array [%s]\n", array[j]);
-		treat_dollar(array[j], 0, 0);
-		printf("array [%s]\n", array[j]);
+		array[j] = treat_dollar(array[j], 0, 0);
 		j++;
 	}
+	return (join_tab(array));
 }
 
 
@@ -43,7 +52,7 @@ char	*parse_env_var(char *word)
 	i = 0;
 	new = word;
 	if (check_occurence(word, '$') > 1)
-		treat_several_dollars(word);
+		return (treat_several_dollars(word));
 	else
 	{
 		while (new[i])
