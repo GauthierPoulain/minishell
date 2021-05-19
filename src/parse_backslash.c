ba59b8cc025da0b8a33c6d0f05771345140426b7
ckurt <ckurt@student.42lyon.fr>
2021-05-19 11:16:32 +0200

    oui

--- src/parse_backslash.c
+++ src/parse_backslash.c
@@ -15,8 +15,8 @@ static size_t	bslash_f_count(char *word, int i)
 
 static int	real_bslash(int nb)
 {
-	if (nb % 2)
-		return (0);
+	// if (nb % 2)
+	// 	return (0);
 	return (((nb / 2) + 1) / 2);
 }
 
@@ -32,9 +32,11 @@ char	*treat_backslash(char *word, int *i, int *trans)
 	char	*new;
 
 	int	back;
+	int	r_back;
 	if (*i == 0)
 	{
 		back = bslash_f_count(word, *i);
+		printf("b_slash count : %d\n", back);
 		if (back % 2 && ft_strlen(word) == (size_t)back)
 		{
 			ft_putstr_fd(2, "Syntax error");
@@ -42,9 +44,13 @@ char	*treat_backslash(char *word, int *i, int *trans)
 		}
 		else
 		{
-			new = ft_calloc_char((real_bslash(back)) + 1, '\\');
+			r_back = real_bslash(back);
+			printf("r_back = %d\n", r_back);
+			new = ft_calloc_char(r_back + 1, '\\');
 			new = ft_strjoin(new, word + back);
 			*i += real_bslash(back);
+			printf("back modul : %d\n", back % 4);
+			printf("r back : %d\n", r_back);
 			if (back % 4 == 1)
 			{
 				*trans = 0;
