3fdd66cc92d5765c87076db7f3a460ae45269de2
Gauthier Poulain <gapoulai@student.42lyon.fr>
2021-06-02 14:28:00 +0200

    fix redirect append

--- src/exec/utils2.c
+++ src/exec/utils2.c
@@ -7,14 +7,14 @@ t_buffer	*get_multiple_input(t_command cmd)
 	res = gc_malloc(sizeof(t_buffer));
 	res->ptr = NULL;
 	res->size = 0;
-	if (g_shell.pipe_output.ptr)
+	if (cmd.file_input && is_a_file(cmd.redirect_path))
+		get_input_part2(cmd, res);
+	else if (g_shell.pipe_output.ptr)
 	{
 		res->ptr = ft_calloc(sizeof(char) * g_shell.pipe_output.size);
 		ft_memcpy(res->ptr, g_shell.pipe_output.ptr, g_shell.pipe_output.size);
 		res->size += g_shell.pipe_output.size;
 	}
-	if (cmd.file_input && is_a_file(cmd.redirect_path))
-		get_input_part2(cmd, res);
 	return (res);
 }
 
@@ -23,6 +23,6 @@ bool	is_operator(char *c)
 	return (!ft_strcmp(c, ";")
 		|| !ft_strcmp(c, ">")
 		|| !ft_strcmp(c, ">>")
-		|| !ft_strcmp(c, "|")
-		|| !ft_strcmp(c, "<"));
+		|| !ft_strcmp(c, "<")
+		|| !ft_strcmp(c, "|"));
 }
