c3b38036f015ac29086929e840c25748e145121a
Gauthier Poulain <gapoulai@student.42lyon.fr>
2021-06-09 11:07:10 +0200

    fix exit code 255

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -33,9 +33,9 @@ int	builtin_exit(char **argv)
 	ret = 0;
 	if (isatty(STDIN_FILENO))
 		ft_putstr("exit\n");
-	if (argv && argv[1] && check_int_overflow(argv[1]))
+	if (argv && argv[1] && *argv[1] && check_int_overflow(argv[1]))
 		ret = 255;
-	else if (argv && argv[1] && ((argv[1][1] && !ft_isdigit(argv[1][1]))
+	else if (argv && argv[1] && *argv[1] && ((argv[1][1] && !ft_isdigit(argv[1][1]))
 			|| (!argv[1][1] && !ft_isdigit(argv[1][0]))))
 		ret = 255;
 	else if (argv && argv[1] && argv[2])
