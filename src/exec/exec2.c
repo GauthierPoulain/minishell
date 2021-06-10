6bf2ddb8b708ff623b6ae0a3e1f41cf985ab79ec
Gauthier Poulain <gapoulai@student.42lyon.fr>
2021-06-10 16:47:26 +0200

    implement tilde

--- src/exec/exec2.c
+++ src/exec/exec2.c
@@ -80,6 +80,8 @@ char	**get_argv(t_ptoken *argv)
 	res = NULL;
 	while (argv && argv->str)
 	{
+		if (!argv->is_escaped)
+			argv->str = ft_strreplace(argv->str, "~",get_env("HOME"));
 		// if (!argv->squotes)
 			replace_env_var(argv);
 		res = tab_add(res, argv->str);
