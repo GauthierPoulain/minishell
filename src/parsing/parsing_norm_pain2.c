0a2489ca9e092f6bc36fe1f974d20c91365f505c
gauthier poulain <gapoulai@student.42lyon.fr>
2021-06-13 03:20:33 +0200

    fix missing spaces

--- src/parsing/parsing_norm_pain2.c
+++ src/parsing/parsing_norm_pain2.c
@@ -39,7 +39,10 @@ void	error_and_bslash(t_token *token, t_ptoken *array, int i)
 void	check_spaces(t_token *token, t_ptoken *array, int i)
 {
 	if ((array + i)->is_in_quotes || (array + i)->is_in_squotes)
+	{
+		(array + i - 1)->str = ft_strjoin((array + i - 1)->str, ft_calloc_char(token->spaces + 1, ' '));
 		(array + i)->spaces = token->spaces;
+	}
 	else if (token->spaces)
 		(array + i)->spaces = 1;
 }
